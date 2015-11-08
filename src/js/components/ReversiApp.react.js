67ed45b7bf244e65e016f8c72295243ae00a1ba0
feng xue <xfsnowind@gmail.com>
2015-11-08 16:41:47 +0100

    add support to topbar

--- src/js/components/ReversiApp.react.js
+++ src/js/components/ReversiApp.react.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React from 'react-native';
-// import TopbarSection from "./TopbarSection.react";
+import TopbarSection from "./TopbarSection.react";
 // import FootbarSection from "./FootbarSection.react";
 import BoardSection from "./BoardSection.react";
 
@@ -9,9 +9,9 @@ let { StyleSheet, View, Text, Component } = React;
 
 class ReversiApp extends Component{
   render () {
-              // <TopbarSection />
               // <FootbarSection />
     return (<View style={{flex: 1}}>
+              <TopbarSection />
               <BoardSection />
           </View>);
   }
--- src/js/components/TopbarSection.react.js
+++ src/js/components/TopbarSection.react.js
@@ -1,10 +1,33 @@
 import React from 'react-native';
 import Immutable from 'immutable';
 import BoardStore from "../stores/BoardStore";
-// import FlipperSection from "./FlipperSection.react";
+import FlipperSection from "./FlipperSection.react";
 import Constants from "../constants/ReversiConstants";
 
-const GridStatus = Constants.get("GridStatus");
+const { StyleSheet, Text, View } = React,
+      StyleValues = Constants.get("Styles"),
+      GridStatus = Constants.get("GridStatus"),
+      boardWidth = StyleValues.get("board_width"),
+      boardMargin = StyleValues.get("board_margin"),
+      styles = StyleSheet.create({
+        "topbar": {
+          marginTop: 40,
+          marginHorizontal: boardMargin + boardWidth / 9,
+          height: boardWidth * 2 / 9,
+          width:  boardWidth * 8 / 9
+        },
+        "topbar__row": {
+          flex: 1,
+          flexDirection: "row",
+          width: boardWidth * 8 / 9,
+          height: boardWidth / 9,
+        },
+        "topbar__grid": {
+          justifyContent: "center",
+          alignItems: 'center',
+          position: "absolute"
+        }
+      });
 
 function getStateFromStores() {
     return {data: Immutable.Map({
@@ -49,32 +72,30 @@ const TopbarSection = React.createClass({
         }
 
         return (
-            <div className="topbar">
-                <table>
-                    <tr>
-                        <td>
-                            <div className="topbar__grid">
-                                <FlipperSection player={GridStatus.get("WHITE")} />
-                            </div>
-                        </td>
-                        <td>{middleText}</td>
-                        <td>
-                            <div className="topbar__grid">
-                                <FlipperSection player={GridStatus.get("BLACK")} />
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>{numWhite}</td>
-                        <td>
-                            <div className="topbar__grid">
-                                <FlipperSection player={player} />
-                            </div>
-                        </td>
-                        <td>{numBlack}</td>
-                    </tr>
-                </table>
-            </div>
+            <View style={styles.topbar}>
+              <View style={styles.topbar__row}>
+                <View style={[styles.topbar__grid, {left: boardWidth / 18}]}>
+                  <FlipperSection player={"WHITE"}/>
+                </View>
+                <View style={[styles.topbar__grid, {left: boardWidth * 2.75 / 9}]}>
+                  <Text>{middleText}</Text>
+                </View>
+                <View style={[styles.topbar__grid, {left: boardWidth * 6.5 / 9}]}>
+                  <FlipperSection player={"BLACK"}/>
+                </View>
+              </View>
+              <View style={styles.topbar__row}>
+                <View style={[styles.topbar__grid, {left: boardWidth * 0.725 / 9}]}>
+                  <Text>{numWhite}</Text>
+                </View>
+                <View style={[styles.topbar__grid, {left: boardWidth * 3.5 / 9}]}>
+                  <FlipperSection player={player} />
+                </View>
+                <View style={[styles.topbar__grid, {left: boardWidth * 6.725 / 9}]}>
+                  <Text>{numBlack}</Text>
+                </View>
+              </View>
+            </View>
         );
     },
 

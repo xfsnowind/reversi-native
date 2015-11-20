59163ad94971b6b5bdf7172819ccd29397c840d0
Feng Xue <feng.xue@nsd.uib.no>
2015-11-20 13:38:13 +0100

    start android

--- src/js/components/ReversiApp.react.js
+++ src/js/components/ReversiApp.react.js
@@ -2,7 +2,7 @@
 
 import React from 'react-native';
 import TopbarSection from "./TopbarSection.react";
-import FootbarSection from "./FootbarSection.react";
+// import FootbarSection from "./FootbarSection.react";
 import BoardSection from "./BoardSection.react";
 
 const { StyleSheet, View, Text, Component } = React,
@@ -10,13 +10,13 @@ const { StyleSheet, View, Text, Component } = React,
           "app": {
             backgroundColor: "#eee8d5",
           }});
+              // <FootbarSection />
 
 class ReversiApp extends Component{
   render () {
     return (<View style={styles.app}>
               <TopbarSection />
               <BoardSection />
-              <FootbarSection />
           </View>);
   }
 };
